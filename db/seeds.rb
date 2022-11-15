28f6a3fe19298c40a7e1ad1bee7f00090e3e7754
Elliott Kan <elliottkan7@gmail.com>
2022-11-15 12:45:43 +1100

    Seed File Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Restaurant.create!({name: "McDonalds", address: "123 Bond Street", phone_number: "0426 263 263", category: "chinese"})
+Restaurant.create!({name: "KFC", address: "11342 Bond Street", phone_number: "042612363", category: "japanese"})
+Restaurant.create!({name: "Burger King", address: "112313243 Bond Street", phone_number: "0426 2623 3", category: "chinese"})
+Restaurant.create!({name: "Subway", address: "123 sehfdh Street", phone_number: "0426 2233", category: "french"})
+Restaurant.create!({name: "Banh Mi", address: "123 Bsdfksd Street", phone_number: "042123 263", category: "italian"})
